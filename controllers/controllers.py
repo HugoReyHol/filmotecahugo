91af728e8e6272213b198017196d1187077fd9d2
Hugo <hugo.reyhol@educa.jcyl.es>
2024-11-14 12:52:45 +0100

    Añadida api get en controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+from odoo.http import Response
+import json
 
+
+class pelicula_controller(http.Controller):
+
+    @http.route('/api/peliculas', auth='public', method=['GET'], csrf=False)
+    def get_peliculas(self, **kw):
+        try:
+            peliculas = http.request.env['filmotecahugo.pelicula'].sudo().search_read([], ['id', 'name', 'color'])
+            res = json.dumps(peliculas, ensure_ascii=False).encode('utf-8')
+            return Response(res, content_type='application/json;charset=utf-8', status=200)
+        except Exception as e:
+            return Response(json.dumps({'error': str(e)}), content_type='application/json;charset=utf-8', status=505)
 
 # class Filmotecahugo(http.Controller):
 #     @http.route('/filmotecahugo/filmotecahugo', auth='public')
